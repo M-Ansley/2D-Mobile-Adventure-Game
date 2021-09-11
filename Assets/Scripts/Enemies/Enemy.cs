51626b71d99f1195b080ba783671c7013897d2b5
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-09-11 23:03:14 +0100

    Enemy Abstract Class Work (1)
    Not technically an abstract class just yet; just a class which two different enemy types inherit from.

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Abstract class (specific enemies inherit from this)
+/// </summary>
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy Properties (Common)")]
+    [SerializeField] protected int health;
+    [SerializeField] protected int speed;
+    protected int gems;
+
+    public void Attack()
+    {
+        Debug.Log("My name is: " + this.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/Enemies/Spider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Attack();
+    }
+    
+}
