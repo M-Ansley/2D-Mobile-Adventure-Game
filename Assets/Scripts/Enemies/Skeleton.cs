9016ca64179380786c94b507c0c868e1cacae51f
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-09-12 19:34:40 +0100

    Skeleton Setup

---
+++ Assets/Scripts/Enemies/Skeleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : Enemy
+{
+    private void Start()
+    {
+        targetPoint = pointA;
+        Initialise();
+    }
+
+    public override void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+
+        FlipSprite();
+        CheckWaypoints();
+        MoveToPoint();
+    }
+}
